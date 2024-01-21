be284c1ba8d53c4ea66ceaa8068684c1ddef2a10
Arthur Chang <arthurc0102@gmail.com>
2024-01-21 13:11:11 +0800

    Update product_list page

--- products/views.py
+++ products/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from products import models as product_models
+
 
 def product_list(request):
-    return render(request, "products/product_list.html")
+    products = (
+        product_models.Product.objects.select_related("category")
+        .prefetch_related("tags")
+        .order_by("name")
+    )
+    return render(request, "products/product_list.html", {"products": products})
