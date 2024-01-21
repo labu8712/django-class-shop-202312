7dda2eca6f634aadc87dc4c6a9dadb2f4ffd1f4d
Arthur Chang <arthurc0102@gmail.com>
2024-01-21 10:16:22 +0800

    Add launch.json and update user admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
-# Register your models here.
+from users import models as user_models
+
+
+@admin.register(user_models.User)
+class UserAdmin(DjangoUserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "email", "password1", "password2"),
+            },
+        ),
+    )
