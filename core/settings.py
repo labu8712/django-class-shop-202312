849c99b23bf2525fb2ae56c4bc213d0315f6e614
Arthur Chang <arthurc0102@gmail.com>
2024-01-21 10:57:30 +0800

    Add product app

--- core/settings.py
+++ core/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django_bootstrap5",
     # Local
     "users",
+    "products",
 ]
 
 MIDDLEWARE = [
---
+++ products/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+
+class Category(models.Model):
+    class Status(models.IntegerChoices):
+        HIDE = 0
+        SHOW = 1
+
+    name = models.CharField(max_length=255, unique=True)
+    status = models.IntegerField(
+        choices=Status.choices,
+        default=Status.SHOW,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    class Status(models.IntegerChoices):
+        HIDE = 0
+        SHOW = 1
+
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.PositiveIntegerField()
+    status = models.IntegerField(
+        choices=Status.choices,
+        default=Status.SHOW,
+    )
+
+    count = models.PositiveBigIntegerField(default=0)
+
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    tags = models.ManyToManyField(Tag)
